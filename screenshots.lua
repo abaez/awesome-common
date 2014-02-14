60246e5b8002aa4e8bb9574033ec143e5f5377ee
Alejandro Baez <Alejan.Baez@gmail.com>
2014-02-14 14:14:30 -0500

    Fixed os.date() call
    Keep forgetting that as a first class function, you shouldn't call as
    variable, else the damn thing will save that state like a closure. -_-

--- screenshots.lua
+++ screenshots.lua
@@ -1,13 +1,10 @@
 --- A quick little screenshot producer for awesome.
-
-
-local timestamp = os.date("%F-%H%M%S")
 local picture_path = "$HOME/Pictures/Screenshots/awesome"
 
 -- @export
 return {
   capture = function()
     os.execute(string.format("import -window root %s/%s.png", picture_path,
-      timestamp))
+       os.date("%F-%H%M%S")))
   end
 }
